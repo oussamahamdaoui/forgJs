6f7c36c3f4034aa2bf1c6580fbf68e44a7d5aa0d
oussama hamdaoui <oussamahamdaoui@live.fr>
2018-12-03 19:27:04 +0100

    tweeter bot added

---
+++ twiterBotTaskRunner/index.js
@@ -0,0 +1,21 @@
+const Twit = require('twit'); // eslint-disable-line
+
+const T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
+});
+const json = require('../package.json');
+
+const message = `Hey there! The new relese ${json.version} of ForgJs is available on #github and #npm.
+Have fun and code with love ❤
+P.S. Im a robot`;
+
+T.post('statuses/update', { status: message }, (err) => {
+  if (!err) {
+    console.log('Grate job');
+  }
+});
