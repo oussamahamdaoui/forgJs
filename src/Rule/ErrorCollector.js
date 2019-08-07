729b5e0f7c0f6d21485d8cc9776a6f1f38b7d30c
Jakub <jakubjezewski@outlook.com>
2019-08-07 14:00:03 +0200

    Docs for ErrorCollector and Rule\util.js (#79)
    * docs for ErrorCollector and Rule\util.js
    
    * fix trailing spaces

--- src/Rule/ErrorCollector.js
+++ src/Rule/ErrorCollector.js
@@ -1,17 +1,37 @@
-
+/**
+ * This object collects errors into array
+ * @class
+ */
 class ErrorCollector {
+  /**
+   * Initializes empty ErrorCollector
+   * @constructor
+   */
   constructor() {
     this.errors = [];
   }
 
+  /**
+   * Adds error to collection
+   *
+   * @param {error} error error added to collection
+   */
   collect(error) {
     this.errors.push(error);
   }
 
+  /**
+   * Removes all errors from this ErrorCollector
+   */
   clear() {
     this.errors = [];
   }
 
+  /**
+   * Returns all collected errors
+   *
+   * @returns {array}
+   */
   get() {
     return this.errors.filter(error => error !== null);
   }
--- src/Rule/util.js
+++ src/Rule/util.js
@@ -1,5 +1,14 @@
 const { isFunction } = require('./../util');
 
+/**
+ * If the error is function, calls this function with two arguments: path and value
+ * and returns the result. If error is string, just returns it.
+ *
+ * @param {Function|String} error representation of error
+ * @param {any} path
+ * @param {any} value
+ * @returns {String}
+ */
 const getErrorFromFunctionOrString = (error, path, value) => {
   if (isFunction(error)) {
     return error(path, value);
@@ -7,6 +16,16 @@ const getErrorFromFunctionOrString = (error, path, value) => {
   return error;
 };
 
+/**
+ * Returns error message for error. If error does not have any property
+ * named key, returns default error message.
+ *
+ * @param {Object} error object error
+ * @param {any} path
+ * @param {any} value
+ * @param {String} key name of error's property which value is error message
+ * @returns {String}
+ */
 const getErrorFromObject = (error, path, value, key) => {
   if (!Object.prototype.hasOwnProperty.call(error, key)) {
     return null; // Here should be the default error message.
